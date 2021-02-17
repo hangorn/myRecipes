2ea5f91cc5a90b3f8a8bc5116e237206c8c07a9a
javier.vaquero <javi_salamanca@hotmail.com>
2021-02-17 11:35:46 +0100

    Cabecera de licencia

--- myrecipes/src/main/java/es/magDevs/myRecipes/dal/be/IngredienteRecetaBean.java
+++ myrecipes/src/main/java/es/magDevs/myRecipes/dal/be/IngredienteRecetaBean.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2014-2020, Javier Vaquero
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * required by applicable law or agreed to in writing, software
+ * under the License is distributed on an "AS IS" BASIS,
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * the License for the specific language governing permissions and
+ * under the License.
+ */
 package es.magDevs.myRecipes.dal.be;
 
 public class IngredienteRecetaBean extends BasicBean {
